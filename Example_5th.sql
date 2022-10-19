cf965f2a98b991c7c5b5b94077da682217c84a2f
alper <109422960+alperkalabali@users.noreply.github.com>
2022-10-19 12:56:23 +0300

    Update Example_5th.sql

--- Example_5th.sql
+++ Example_5th.sql
@@ -11,7 +11,7 @@ ORDER BY length ASC
 OFFSET 5
 LIMIT 5;
 
---customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+-- customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
 SELECT * FROM customer
 WHERE store_id = 1
 ORDER BY last_name DESC
